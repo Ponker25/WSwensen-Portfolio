3853f8dc053188fc15a0501417c841bee4af1072
William Andrew Swensen <williamswensen86@gmail.com>
2018-12-18 19:24:43 -0700

    flip card styling

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -5,13 +5,17 @@ const PortfolioCard = props => (
   
   <div className="flip-card">
     <div className="flip-card-inner" alt={props.name}>
-      <div className="flip-card-front" id={props.id} />
-      <img alt={props.name} src={window.location + `images/${props.image}`} />   
+      <div className="flip-card-front" id={props.id}> 
+        <img alt={props.name} src={window.location + `images/${props.image}`} />  
+        </div> 
           <div className="flip-card-back"id={props.id}
           alt={props.name}
           value={props.id}
-          onClick={props.github}
+          onClick={props.github} href={props.link}
           >
+        <p> 
+          {props.name}
+        </p>
       </div>
     </div>
   </div>
