ff10e5d320f1de22aaa343614a348eb1bef41354
William Andrew Swensen <williamswensen86@gmail.com>
2018-12-18 18:47:14 -0700

    portfolio card flip

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -3,18 +3,21 @@ import "./PortfolioCard.css";
 
 const PortfolioCard = props => (
   
-  <div className="card">
-    <div className="img-container" id={props.id}
-      value={props.id}
-      onClick={props.github}
-      >
-      <img alt={props.name} src={window.location + `images/${props.image}`} />
-      
+  <div className="flip-card">
+    <div className="flip-card-inner" alt={props.name}>
+      <div className="flip-card-front" id={props.id} />
+      <img alt={props.name} src={window.location + `images/${props.image}`} />   
+          <div className="flip-card-back"id={props.id}
+          alt={props.name}
+          value={props.id}
+          onClick={props.github}
+          >
+      </div>
     </div>
-
   </div>
 );
 
 
 
 export default PortfolioCard;
+
